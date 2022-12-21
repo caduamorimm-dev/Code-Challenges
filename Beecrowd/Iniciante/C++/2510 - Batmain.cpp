205db1917f02511ceac360197e09260963f572ea
Carlos Eduardo Amorim <86995782+caduamorimm-dev@users.noreply.github.com>
2022-12-21 10:32:41 -0300

    Create      2510 - Batmain.cpp

---
+++ Beecrowd/Iniciante/C++/2510 - Batmain.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int T;
+    string vilao;
+    
+    cin >> T;
+    
+    for (int i = 0; i < T; i++){
+        cin >> vilao;
+        cout << "Y" << endl;
+    }
+    
+    return 0;
+}

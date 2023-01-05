a2f8021bf4ef3ca4eea076d1e910683cbf0d5c88
Carlos Eduardo Amorim <86995782+caduamorimm-dev@users.noreply.github.com>
2023-01-05 12:35:24 -0300

    Create 1143 - Quadrado e ao Cubo.cpp

---
+++ Beecrowd/Iniciante/C++/1143 - Quadrado e ao Cubo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+    
+  int N;
+    
+  cin >> N;
+ 
+  for(int i = 0; i < a; i++){
+     cout << i+1 << " " << pow(i+1,2) << " " << pow(i+1,3) << endl;
+    }
+    
+    return 0;
+}
